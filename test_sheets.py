3e9c66313c5f1cbf06b8691f904004dcacfa15dd
bjmstoredata-arch <bjmstoredata@gmail.com>
2025-08-12 15:07:51 -0400

    Update test_sheets.py

--- test_sheets.py
+++ test_sheets.py
@@ -1,20 +1,15 @@
 import streamlit as st
-import gspread
 from google.oauth2.service_account import Credentials
+import gspread
 
-SCOPE = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
 
-creds = Credentials.from_service_account_info(st.secrets["google_service_account"], scopes=SCOPE)
-client = gspread.authorize(creds)
+creds = Credentials.from_service_account_info(
+    st.secrets["google_service_account"],
+    scopes=SCOPE
+)
 
-# Print the email
-st.write("Service account email:", creds.service_account_email)
-
-# List all accessible spreadsheets
-files = client.list_spreadsheet_files()
-if files:
-    st.write("Sheets the service account can access:")
-    for f in files:
-        st.write(f["name"], "-", f["id"])
-else:
-    st.write("No spreadsheets found. Make sure the service account has access.")
+st.write("Service Account Email:", creds.service_account_email)
