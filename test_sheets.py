da7f368c8091848330e76b0b28d18f06437c4324
bjmstoredata-arch <bjmstoredata@gmail.com>
2025-08-12 14:13:31 -0400

    Update test_sheets.py

--- test_sheets.py
+++ test_sheets.py
@@ -15,7 +15,7 @@ creds = Credentials.from_service_account_info(
 )
 
 client = gspread.authorize(creds)
-SHEET_NAME = "ClientsDatabase"  # Change to your Google Sheet name
+SHEET_NAME = "CarPartsDatabase"  # Change to your Google Sheet name
 sheet = client.open(SHEET_NAME).sheet1
 
 data = sheet.get_all_records()
@@ -24,3 +24,4 @@ df = pd.DataFrame(data)
 st.title("Car Parts Manager")
 st.write("Current Client Data from Google Sheets:")
 st.dataframe(df)
+
